cd39c56a15c659ecb6e1b940e252b8de7052739d
rihappytangara <rihappy.tangara@gmail.com>
2026-06-19 16:14:49 -0400

    Refactor date formatting in cadastro.js
    Removed redundant date formatting logic.

--- js/cadastro.js
+++ js/cadastro.js
@@ -232,13 +232,6 @@ if (dataBr) {
       `${partes[4]}:${partes[5]}:${partes[6]}`;
   }
 }
-     
-if (partes) {
-
-  dataIso =
-    `${partes[3]}-${partes[2]}-${partes[1]} ` +
-    `${partes[4]}:${partes[5]}:${partes[6]}`;
-}
 
 console.log("dataBr:", dataBr);
 console.log("dataIso:", dataIso);
